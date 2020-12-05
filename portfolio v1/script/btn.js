ae9ce96558e9b49c866ddc7ab3640ac2987c0643
letirien <43708815+letirien@users.noreply.github.com>
2020-12-05 17:33:41 +0100

    Add files via upload

---
+++ portfolio v1/script/btn.js
@@ -0,0 +1,17 @@
+
+var btn = document.querySelector ('.btn');
+var nav = document.querySelector('.nav');
+
+btn.onclick = function(){
+    nav.classList.toggle('nav_open');
+}
+
+nav.addEventListener('click', function(e) {
+    // console.log(e);
+ if(e.target.tagName === "A") {
+     nav.classList.remove('nav_open');
+
+    // console.log(e.target.tagName);
+ }
+
+});
